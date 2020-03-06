09d5448e6a1404c0afeed45c874075468d4acd57
nickjagar <mitsuya08@gmail.com>
2020-03-06 23:16:35 +0900

    AVリストも出した　イニシャルを入れれば100人出る　これがやりたかった

--- suke_dmm.py
+++ suke_dmm.py
@@ -10,13 +10,15 @@ import requests,bs4,csv,pprint,json
 # キーセット
 APIID = 'wn2k2FSt6C1QFqcLLxgG'
 AFFILIATEID = 'nickjagar-990'
-KEYWORD = 'あ'
+initial = 'う'
+KEYWORD = 'いい'
 hitnum = '100'
 outformat = 'json'
 
 # 出力ファイル名
 output01_json = 'avlist.json'
 gazou_folder = '/home/mitsuya/Pictures/avact/'
+avlist = 'avlist.txt'
 
 '''
 
@@ -24,7 +26,7 @@ memo:
 https://api.dmm.com/affiliate/v3/ActressSearch?api_id=[APIID]&affiliate_id=[アフィリエイトID]&keyword=%e3%81%82%e3%81%95%e3%81%bf&gte_bust=90&lte_waist=60&sort=-bust&hits=10&offset=10&output=json
 
 '''
-url = 'https://api.dmm.com/affiliate/v3/ActressSearch?api_id='+APIID+'&affiliate_id='+AFFILIATEID+'&keyword='+KEYWORD+'&hits='+hitnum+'&output='+outformat
+url = 'https://api.dmm.com/affiliate/v3/ActressSearch?api_id='+APIID+'&affiliate_id='+AFFILIATEID+'&initial='+initial+'&hits='+hitnum+'&output='+outformat
 
 print(url)
 #DMMのAPIからreqオブジェクト取得
@@ -51,13 +53,19 @@ for d in actress:
     actress_name = d.get('name')
     bustsize = d.get('bust')
     cupsize = d.get('cup')
+    waistsize = d.get('waist')
+    hipsize = d.get('hip')
     # 画像取得（値がないときは100を返す）
     a_imageURL = d.get('imageURL',100)
-    # 女優名とバストサイズとcupサイズ
-    print(actress_name+':'+str(bustsize)+' '+str(cupsize))
+    # 女優名と3サイズとcupサイズ
+    nsize = actress_name+':'+str(bustsize)+' '+str(waistsize)+' '+str(hipsize)+' '+str(cupsize)+'cup'
+    #print(actress_name+':'+str(bustsize)+' '+str(waistsize)+' '+str(hipsize)+' '+str(cupsize)+'cup')
+    print(nsize)
+    with open(avlist,"a") as al:
+        al.write(nsize+'\n')
 
     if not a_imageURL == 100:
-        print(type(a_imageURL))
+        # print(type(a_imageURL))
         a_image = a_imageURL['large']
         print(a_image)
         # URLからjpgファイル名を抽出
